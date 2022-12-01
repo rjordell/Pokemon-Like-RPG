a59019af7d661110d8bfc8469376245a6c7d0436
aphan079 <aphang355@gmail.com>
2022-11-30 23:38:11 -0800

    Update Person.cpp
    Merged from the other Person.cpp file

--- src/Person.cpp
+++ src/Person.cpp
@@ -20,3 +20,48 @@ void Player::catchPokemon(Pokemon& target) {
     }
     cout << "You have no space to add " << target->getPokemonName() << " to your team. You let " << target->getPokemonName() << " go..." << endl;\
 }
+
+void Player::addToTeam(Pokemon newMember)
+{
+    for(int i = 0; i < team.size(); i++)
+    {
+        if(team.at(i) == nullptr)
+        {
+            team.at(i) = newMember;
+            cout << "Added " << newMember << " to the team successfully~" << endl;
+            cout << "Do you want to see your team?" << endl;
+            string choice;
+            if(choice == "YES" || choice == "yes" || choice == "Yes")
+            {
+                for(int i = 0 i < team.size(); i++)
+                {
+                    if(i != 0)
+                    {
+                        cout << ", ";
+                    }
+                    cout << team.at(i);
+                    
+                }
+                break;
+            }
+            else
+            {
+                break;
+            }
+            
+        }
+    }
+    //team.push_back(target->getPokemonName());
+}
+
+/*void Player::viewTeam() //Ended up adding this to the addToTeam Function
+{
+    for(int i = 0 i < team.size(); i++)
+    {
+        if(i != 0)
+        {
+            cout << ", ";
+        }
+        cout << team.at(i);
+    }
+}*/
