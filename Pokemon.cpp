91bc35ea2fc11231bc205b9e439a3209f2cbc353
jramo165 <jramo165@ucr.edu>
2022-11-30 20:10:00 -0800

    Added Pokemon constructor and fixed issues with having Pokemon types

---
+++ Pokemon.cpp
@@ -0,0 +1,13 @@
+#include "Pokemon.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Pokemon::Pokemon() {
+    this->ownerName = "";
+    this->name = "";
+    this->experience = 0;
+    this->health = 100;
+    
+}
---
+++ Pokemon.h
@@ -0,0 +1,26 @@
+#ifndef POKEMON_H
+#define POKEMON_H
+#include <string>
+using namespace std;
+
+class Pokemon {
+ private:
+    string ownerName;
+    string name;
+    enum type {NORMAL, FIRE, WATER, GRASS, ELECTRIC, ICE, FIGHTING, POISON, GROUND, FLYING, PSYCHIC, BUG, ROCK, GHOST, DRAGON, DARK, STEEL};
+    type PokemonType;
+    int experience;
+    int health;
+    friend class Person;
+ public:
+    Pokemon();
+    void attack(Pokemon enemy);
+    void levelUp();
+    void addExp();
+    void changeHealth(int difference); //used for being damaged/healed
+    void setPokemonName();
+    string getPokemonName();
+    int getHealth();
+};
+
+#endif
