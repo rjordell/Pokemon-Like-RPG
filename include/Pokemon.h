84c5a5f1863f21e96ea700bff6da9540663787c6
rjordell <rjordell@gmail.com>
2022-12-01 12:36:35 -0800

    Improved leveling system

--- include/Pokemon.h
+++ include/Pokemon.h
@@ -9,17 +9,20 @@ class Pokemon {
       string name;
       string nickName;
     
+      
+      friend class Person;
+   public:
       int health;
       int maxHealth;
       int level;
-      int experience;
-      friend class Person;
-   public:
+      int exp;
+      int expNeeded = 100;
+      int atkDmg = 20;
       enum type {FIRE, WATER, EARTH};
       type PokemonType;
       const char *typeNames[3] = { "Fire", "Water", "Earth" };
       Pokemon();
-      void assign(string name, type PokemonType, int health, int maxHealth, int level, int experience);
+      void assign(string name, type PokemonType, int health, int maxHealth, int level, int exp, int atkDmg);
       void attack(Pokemon enemy);
       void changeHealth(int difference); //used for being damaged/healed
       void levelUp();
--- src/Pokemon.cpp
+++ src/Pokemon.cpp
@@ -6,38 +6,40 @@
 
 using namespace std;
 
+string bold = "\e[1m";
+string reset = "\e[0m\e[39m\e[49m";
+
 Pokemon::Pokemon() {
-    // this->name = "";
-    // this->PokemonType = FIRE;
-    // this->nickName = "";
-    // this->health = 100;
-    // this->maxHealth = 100;
-    // this->level;    
-    // this->experience = 0;
+
 }
 
-void Pokemon::assign(string name, type PokemonType, int health, int maxHealth, int level, int experience){
+void Pokemon::assign(string name, type PokemonType, int health, int maxHealth, int level, int exp, int atkDmg){
     this->name = name;
     this->PokemonType = PokemonType;
     this->health = health;
     this->maxHealth = maxHealth;
     this->level = level;
-    this->experience = experience;
+    this->exp = exp;
+    this->atkDmg = atkDmg;
 }
 
 void Pokemon::levelUp() {
-    this->level++;
-    cout << this->name << " leveled up. They are level " << this->level << " now!" << endl;
+    level++;
+    expNeeded += 125;
+    maxHealth += 5;
+    atkDmg += 10;
+    cout << bold << this->nickName << reset << " leveled up. They are now level " << level << "!" << endl;
 }
 
-void Pokemon::addExp()
-{
-    this->experience += 25;
-    cout << "You have gained 25 experience!";
-    if(experience % 25 == 0) {
+void Pokemon::addExp() {
+    exp += 25;
+    cout << endl << bold << this->nickName << reset << " has gained 25 experience points!" << endl;
+    if(exp >= expNeeded) {
         levelUp();
+    } else {
+        cout << bold << this->nickName << reset << " is now " << expNeeded-exp << " points away from leveling up!" << endl;
     }
-    cout << "You have " << this->experience << "experience!" << endl;
+    
 }
 
 void Pokemon::setPokemonNickName() 
@@ -50,48 +52,24 @@ void Pokemon::setPokemonNickName()
     if (choice == 'y') {
         cout << "Please type in a nickname: ";
         cin >> pokeNick;
-        cout << endl << "Great name! Have fun on your journey with "<<pokeNick << "!" << endl;
+        this->nickName = pokeNick;
+        cout << endl << "Great name! Have fun on your journey with " << this->nickName << "!" << endl;
     } else {
+        this->nickName = this->name;
         cout << endl << "Have fun on your journey with "<<this->name << "!" << endl;
     }
-    this->nickName = pokeNick;
 }
 
 string Pokemon::getPokemonName() {
-    return this->name;
+    return this->nickName;
 }
 
-// void Pokemon::displayInfo() {
-//     char choice;
-//     cout << "Would you like to know more about " << this->name << "?" << endl;
-//     cout << "Type 'y' for yes or 'n' for no: ";
-//     cin >> choice;
-//     if (choice == 'y') {
-//         if (this->nickName == "") {
-//             cout << "Nickname: N/A" << endl;
-//         }
-//         else {
-//             cout << "Nickname: " << this->nickName << endl;
-//         }
-//         cout << "Type: " << this->type << endl;
-//         cout << "Health: " << this->health << endl;
-//         cout << "Level: " << this->level << endl;
-//         cout << "Experience: " << this->experience << endl;
-//     }
-//     else {
-//         cout << "Okay. Good luck!" << endl;
-//     }
-// }
-
 void Pokemon::displayInfo() {
-    string bold = "\e[1m";
-    string reset = "\e[0m\e[39m\e[49m";
-    if (this->nickName == "") {
-        cout << bold << "Lvl " << this->level << ". "<< this->name << reset << endl;
-        cout << "    " << typeNames[this->PokemonType] << " type " << this->name << endl << "    HP: " << this->health << "/" << this->maxHealth << " Exp: " << this->experience << "/100" << endl;
-    }
-    else {
-        cout << bold << "Lvl " << this->level << ". "<< this->nickName << reset << endl;
-        cout << "    " << typeNames[this->PokemonType] << " type " << this->name << endl << "    HP: " << this->health << "/" << this->maxHealth << " Exp: " << this->experience << "/100" << endl;
-    }
+    cout << bold << "Lvl " << this->level << ". "<< this->nickName << reset << endl;
+    cout << "    " << typeNames[this->PokemonType] << " type " << this->name << endl;
+    cout << "    HP: " << health << "/" << maxHealth << ", Attack: " << atkDmg << ", Exp: " << exp << "/" << expNeeded << endl;
+
+    // cout << bold << "Lvl " << this->level << ". "<< this->nickName << reset << endl;
+    // cout << "    " << typeNames[this->PokemonType] << " type " << this->name << endl;
+    // cout << "    HP: " << this->health << "/" << this->maxHealth << ", Attack: " << this->atkDmg << ", Exp: " << this->exp << "/" << this->expNeeded << endl;
 }
